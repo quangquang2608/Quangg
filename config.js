105cca890e8fbaca78881458612ac68035675580
quangquang2608 <148530314+quangquang2608@users.noreply.github.com>
2023-10-20 15:06:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '';
+
+const eventName = 'Hello :>';
+const titleCard = 'Chào cậu';
+const contentCard = 'Có một ngày 20/10 thật vui vẻ nhé';
+
+const giftImage = '......png';
+// const base64 = '';
+// const giftImageBase64 = "data:image/png;base64, " + base64;
